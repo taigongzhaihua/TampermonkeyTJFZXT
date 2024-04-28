f69cd1cbe1095c1e5a0fb455e9b1e1e24b15fe0a
Peter Jones <zzy944541960@outlook.com>
2024-04-28 22:26:30 +0800

    增加metajs的部署

--- uploadToJianguo.js
+++ uploadToJianguo.js
@@ -10,13 +10,17 @@ const client = createClient(
     { username, password }
 );
 
-const localFilePath = "dist/your-script.user.js"; // 本地文件路径
-const remoteFilePath = "TampermonkeyTJFZXT/your-script.user.js"; // 坚果云中的文件路径
+const userJsFilePath = "dist/your-script.user.js"; // 本地文件路径
+const metaJsFilePath = "dist/your-script.meta.js"; // 本地文件路径
+const remoteUserJsFilePath = "TampermonkeyTJFZXT/your-script.user.js"; // 坚果云中的文件路径
+const remoteMetaJsFilePath = "TampermonkeyTJFZXT/your-script.meta.js"; // 坚果云中的文件路径
 
 async function uploadFile() {
     try {
-        const fileContents = fs.createReadStream(localFilePath);
-        await client.putFileContents(remoteFilePath, fileContents, { overwrite: true });
+        const UserJsFileContents = fs.createReadStream(userJsFilePath);
+        const metaJsFileContents = fs.createReadStream(metaJsFilePath);
+        await client.putFileContents(remoteUserJsFilePath, UserJsFileContents, { overwrite: true });
+        await client.putFileContents(remoteMetaJsFilePath, metaJsFileContents, { overwrite: true });
         console.log("File uploaded successfully!");
     } catch (error) {
         console.error("Error uploading file:", error);
