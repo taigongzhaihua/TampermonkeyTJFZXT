2ad702066cb9d8e72d680155aec5e2e7cf1e5bec
GitHub Action <action@github.com>
2024-04-30 16:38:59 +0000

    Automated build commit

--- dist/History/0.1.14/script-0.1.14.user.js
+++ dist/History/0.1.14/script-0.1.14.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         体检系统辅助
 // @namespace    http://tampermonkey.net/
-// @version      0.1.15
+// @version      0.1.14
 // @description  监控特定元素属性的变化，并根据变化执行相应的操作。
 // @author       太公摘花
 // @match        https://wx.changx.com/*
@@ -129,7 +129,15 @@
             "尿潜血",
             "白细胞"
         ];
-        selectDropdownOption(urinalysisItems[0], '-')；
+        selectDropdownOption(urinalysisItems[0], '-').then(() => {
+            selectDropdownOption(urinalysisItems[1], '-').then(() => {
+                selectDropdownOption(urinalysisItems[2], '-').then(() => {
+                    selectDropdownOption(urinalysisItems[3], '-').then(() => {
+                        selectDropdownOption(urinalysisItems[4], '-');
+                    });
+                });
+            });
+        });
     }
 
     /**
