f82c4536a7df603adcefdd7d668c94f50dd2ec3e
Peter Jones <zzy944541960@outlook.com>
2024-05-01 16:48:33 +0800

    暂时恢复追踪

---
+++ dist/script-last.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         体检系统辅助
+// @namespace    http://tampermonkey.net/
+// @version      0.1.18
+// @description  监控特定元素属性的变化，并根据变化执行相应的操作。
+// @author       太公摘花
+// @match        https://wx.changx.com/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.7.1.js
+// @updateURL    https://raw.githubusercontent.com/taigongzhaihua/TampermonkeyTJFZXT/main/dist/script-last.meta.js
+// @downloadURL  https://raw.githubusercontent.com/taigongzhaihua/TampermonkeyTJFZXT/main/dist/script-last.user.js
+// ==/UserScript==
