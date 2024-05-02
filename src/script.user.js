cd0b25d22fe352c5ae1f4d3bbb10cd35df2b385a
Peter Jones <zzy944541960@outlook.com>
2024-05-03 02:39:41 +0800

    实现尿常规自动填写

--- src/script.user.js
+++ src/script.user.js
@@ -18,16 +18,42 @@
     let tab1Observer = null; // 标签页1的观察者对象
     let tab2Observer = null; // 标签页2的观察者对象
 
-    // 监控对话框的显示与隐藏状态
-    $(document).ready(() => {
-        monitorDialog('.el-dialog__wrapper.page-dialog.all-test-dialog');
-    });
+    /**
+     * 设置元素属性变化的观察者
+     * 
+     * @param {string} selector - jQuery选择器，用于定位需要监控的元素
+     * @param {Object} config - 观察者配置对象
+     * @param {Function} callback - 当属性变化时执行的回调函数
+     * @returns {MutationObserver} - 返回创建的MutationObserver对象
+     * @example
+     * setupObserver('#tab-0', { attributes: true, attributeFilter: ['tabindex'] }, performTab0Actions);
+     * setupObserver('#tab-1', { attributes: true, attributeFilter: ['tabindex'] }, performTab1Actions);
+     * setupObserver('#tab-2', { attributes: true, attributeFilter: ['tabindex'] }, performTab2Actions);
+     * ...
+     * @see {@link https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver}
+     */
+    function setupObserver(selector, config, callback) {
+        const element = $(selector);
+        if (element.length) {
+            const observer = new MutationObserver((mutations) => {
+                mutations.forEach((mutation) => callback($(mutation.target)));
+            });
+
+            observer.observe(element[0], config);
+            return observer;
+        } else {
+            setTimeout(() => setupObserver(selector, config, callback), 500);
+        }
+    }
 
     /**
      * 监控对话框的显示与隐藏状态
      * 
      * @param {string} selector - jQuery选择器，用于定位需要监控的元素
      * @returns {void} - 无返回值
+     * @example
+     * monitorDialog('.el-dialog__wrapper.page-dialog.all-test-dialog');
+     * ...
      */
     function monitorDialog(selector) {
         const observerConfig = {
@@ -47,17 +73,182 @@
         });
     }
 
-    function setupObserver(selector, config, callback) {
-        const element = $(selector);
-        if (element.length) {
-            const observer = new MutationObserver((mutations) => {
-                mutations.forEach((mutation) => callback($(mutation.target)));
+    /**
+     * 监控单个元素属性变化
+     * 
+     * @param {string} selector - jQuery选择器，用于定位需要监控的元素
+     * @param {string} attribute - 监控的属性
+     * @param {Function} action - 当属性变化符合条件时执行的操作
+     * @returns {MutationObserver|null} 返回创建的MutationObserver对象或null
+     * @example
+     * setupElementObserver('#tab-0', 'tabindex', 0, performTab0Actions);
+     * setupElementObserver('#tab-1', 'tabindex', 0, performTab1Actions);
+     * setupElementObserver('#tab-2', 'tabindex', 0, performTab2Actions);
+     * ...
+     */
+    function setupElementObserver(selector, attribute, value, action) {
+        return waitForElement(selector).then((element) => {
+            if (element.attr(attribute) === value) {
+                action();
+            }
+            return setupObserver(selector, {
+                attributes: true,
+                attributeFilter: [attribute]
+            }, (element) => {
+                if (element.attr(attribute) === value) {
+                    action();
+                }
             });
+        });
 
-            observer.observe(element[0], config);
-            return observer;
+    }
+
+    /**
+     * 模拟指定标题的下拉框选择指定选项
+     * 
+     * @param {string} title - 下拉框标题
+     * @param {string} option - 选项
+     * @returns {Promise<{success: boolean, message: string}>} - 返回一个Promise对象，包含操作结果
+     * @throws {Error} - 如果参数不符合要求，将抛出异常
+     * @example
+     * selectDropdownOption('尿蛋白', '阴性').then(result => {
+     *   if (result.success) {
+     *    console.log(result.message);
+     *  } else {
+     *   console.error(result.message);
+     * }
+     * });
+     */
+    async function selectDropdownOption(title, option) {
+        if (typeof title !== 'string' || typeof option !== 'string') {
+            throw new Error('标题和选项参数必须是字符串。');
+        }
+
+        try {
+            console.log(`正在查找标题为 "${title}" 的下拉框。`);
+            const labelDiv = await waitFor(() => $(`div.el-form-item__content`).filter(function () {
+                // 忽略空格和大小写，查找匹配的选项
+                return $.trim($(this).text()) === title;
+            }));
+
+            if (labelDiv.length === 0 || labelDiv.find('input').val() !== '') {
+                return { success: false, message: `未找到未选值的标题为"${title}"的下拉框。` };
+            }
+
+            console.log(`找到未选值的下拉框，触发 "${title}" 下拉菜单。`);
+            labelDiv.children().first().click();
+
+            console.log(`正在等待 "${title}" 下拉菜单选项出现。`);
+            const placementDiv = await waitFor('div[x-placement="bottom-start"]');
+            const listItem = placementDiv.find('li').filter((i, elem) => $.trim($(elem).text()) === option);
+
+            if (listItem.length === 0) {
+                return { success: false, message: `未找到选项 "${option}"。` };
+            }
+
+            listItem.click();
+            await waitFor(() => labelDiv.find('input'), input => input.val() === option && placementDiv.css('display') === 'none');
+
+            console.log(`选项 "${option}" 已成功选择，下拉菜单已关闭。`);
+            return { success: true, message: `选项 "${option}" 已成功选择。` };
+        } catch (error) {
+            console.error(`在选择 ${title} 时发生错误: ${error.message}`);
+            return { success: false, message: error.message };
+        }
+    }
+
+    /**
+     * 
+     * 等待元素出现，并满足条件
+     * 
+     * @summary 等待元素出现，并满足条件
+     * 
+     * @param {string|Function} selector - jQuery选择器或返回jQuery对象的函数 
+     * @param {Function} condition - 检查条件的函数
+     * @param {number} timeout - 超时时间
+     * @param {number} interval - 检查间隔
+     * @returns {Promise<jQuery>} - 返回一个Promise对象，包含满足条件的jQuery对象
+     * @throws {Error} - 如果参数不符合要求，将抛出异常
+     * @example
+     * waitFor('div#myDiv', $div => $div.is(':visible')).then($div => {
+     *    console.log('元素已可见。');
+     * });
+     * waitFor(() => $('div#myDiv'), $div => $div.is(':visible')).then($div => {
+     *   console.log('元素已可见。');
+     * }
+     * ...
+     * @see {@link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise}
+     * @see {@link https://api.jquery.com/jQuery/}
+     */
+    async function waitFor(selector, condition = () => true, timeout = 30000, interval = 100) {
+
+        return new Promise((resolve, reject) => {
+            if (typeof selector !== 'string' && typeof selector !== 'function') {
+                reject(new Error('参数selector必须是字符串或返回jQuery对象的函数。'));
+            }
+            if (typeof condition !== 'function') {
+                reject(new Error('condition不是一个有效的function。'));
+            }
+            if (typeof timeout !== 'number' || typeof interval !== 'number') {
+                reject(new Error('timeout和interval必须为number。'));
+            }
+
+            const startTime = Date.now();
+
+            // 检查条件是否满足
+            function checkCondition() {
+                let $element = typeof selector === 'function' ? selector() : $(selector);
+                if (!($element instanceof jQuery)) {
+                    reject(new Error("选择器或函数没有返回一个jQuery对象。"));
+                }
+
+                // 检查jQuery选择器是否存在且满足条件，满足则返回一个jQuery对象，否则继续等待，直到超时
+                if ($element.length > 0 && condition($element)) {
+                    resolve($element);
+                } else if (Date.now() - startTime > timeout) {
+                    reject(new Error('等待条件满足超时。'));
+                } else {
+                    setTimeout(checkCondition, interval);
+                }
+            }
+
+            checkCondition();
+        });
+    }
+
+    /**
+     * 模拟点击指定选择器的每一个元素
+     * 
+     * @param {string} selector - jQuery选择器，指定需要模拟点击的元素
+     * @returns {void} - 无返回值
+     * @example
+     * simulateClick('input[type="radio"]');
+     * simulateClick('input[type="checkbox"]');
+     * ...
+     */
+    function simulateClick(selector) {
+        $(selector).each(function () {
+            var $label = $(this).closest('label');
+            clickUncheckedLabel($label)
+        });
+    }
+
+    /**
+     * 点击未被选中的标签
+     * 
+     * @param {jQuery} label - jQuery对象，表示需要点击的标签
+     * @returns {void} - 无返回值
+     * @example
+     * clickUncheckedLabel($('label'));
+     * clickUncheckedLabel($('label').first());
+     * clickUncheckedLabel($('label').eq(1));
+     * ...
+     */
+    function clickUncheckedLabel(label) {
+        if (!label.parents("tr").find('label.is-checked').length) {
+            label.click(); // 如果没有选中的标签，进行点击
         } else {
-            setTimeout(() => setupObserver(selector, config, callback), 500);
+            console.log('已存在被选中的标签，跳过当前操作');
         }
     }
 
@@ -93,52 +284,25 @@
     }
 
     /**
-     * 监控单个元素属性变化
-     * 
-     * @param {string} selector - jQuery选择器，用于定位需要监控的元素
-     * @param {string} attribute - 监控的属性
-     * @param {Function} action - 当属性变化符合条件时执行的操作
-     * @returns {MutationObserver|null} 返回创建的MutationObserver对象或null
-     */
-    function setupElementObserver(selector, attribute, value, action) {
-        return waitForElement(selector).then((element) => {
-            if (element.attr(attribute) === value) {
-                action();
-            }
-            return setupObserver(selector, {
-                attributes: true,
-                attributeFilter: [attribute]
-            }, (element) => {
-                if (element.attr(attribute) === value) {
-                    action();
-                }
-            });
-        });
-
-    }
-
-    /**
      * 执行标签页0的相关操作
      * 
      * @returns {void} - 无返回值
      */
     async function performTab0Actions() {
-        let urinalysisItems = [
-            "尿蛋白",
-            "尿糖",
-            "尿酮体",
-            "尿潜血",
-            "白细胞"
-        ];
-        for (let i = 0; i < urinalysisItems.length; i++) {
-            // 等待每次下拉选项选择完成后再进行下一次选择
-            await selectDropdownOption(urinalysisItems[i], "-")
-                .then(result => {
-                    console.log(`成功选择 ${urinalysisItems[i]}: ${result}`);
-                })
-                .catch(error => {
-                    console.error(`在选择 ${urinalysisItems[i]} 时发生错误: ${error.message}`);
-                });
+        let urinalysisItems = ["尿蛋白", "尿糖", "尿酮体", "尿潜血", "白细胞"];
+
+        for (let item of urinalysisItems) {
+            try {
+                let result = await selectDropdownOption(item, "-");
+                if (result.success) {
+                    console.log(`"${item}"选值成功：${result.message}`);
+                } else {
+                    console.log(`"${item}"未能成功选值：${result.message}`);
+                }
+
+            } catch (error) {
+                console.error(`在选择 ${item} 时发生错误: ${error.message}`);
+            }
         }
     }
 
@@ -163,129 +327,9 @@
         simulateClick('div#pane-2 input[type="radio"][value="0"]');
     }
 
-    /**
-     * 模拟点击指定选择器的每一个元素
-     * 
-     * @param {string} selector - jQuery选择器，指定需要模拟点击的元素
-     * @returns {void} - 无返回值
-     */
-    function simulateClick(selector) {
-        $(selector).each(function () {
-            var $label = $(this).closest('label');
-            clickUncheckedLabel($label)
-        });
-    }
-
-    /**
-     * 点击未被选中的标签
-     * 
-     * @param {jQuery} label - jQuery对象，表示需要点击的标签
-     * @returns {void} - 无返回值
-     */
-    function clickUncheckedLabel(label) {
-        if (!label.parents("tr").find('label.is-checked').length) {
-            label.click(); // 如果没有选中的标签，进行点击
-        } else {
-            console.log('已存在被选中的标签，跳过当前操作');
-        }
-    }
-
-    /**
-     * 定义一个异步函数 selectDropdownOption，
-     * 用于模拟点击指定下拉框标签的指定选项。
-     * 
-     * @async
-     * @param {string} title - 标签名
-     * @param {string} option - 要点击的选项
-     * @returns {void} - 无返回值
-     */
-    async function selectDropdownOption(title, option) {
-        // 检查 title 和 option 是否是字符串，如果不是，抛出错误
-        if (typeof title !== 'string' || typeof option !== 'string') {
-            throw new Error('标题和选项参数必须是字符串。');
-        }
-
-        try {
-            console.log(`正在查找标题为 "${title}" 的元素。`);
-            const labelDiv = await waitForElement(`div.el-form-item__content:contains('${title}')`);
-
-            // 如果没有找到元素，抛出错误
-            if (labelDiv.length === 0) {
-                throw new Error(`未找到标题为"${title}"的元素。`);
-            }
-
-
-            if (labelDiv.find('input').attr('value') === ''){
-                console.log(`正在触发 "${title}" 下拉菜单。`);
-                labelDiv.children().first().click();
-            } else {
-                console.log(`"${title}" 下拉框已存在值，跳过当前操作。`);
-                return;
-            }
-
-            console.log(`正在等待 "${title}" 下拉菜单选项出现。`);
-            const placementDiv = await waitForElement('div[x-placement="bottom-start"]');
-            const listItem = placementDiv.find('li.el-select-dropdown__item').filter(function () {
-                // 忽略空格和大小写，查找匹配的选项
-                return $.trim($(this).text()) === option;
-            });
-
-            // 如果没有找到选项，抛出错误
-            if (listItem.length === 0) {
-                throw new Error(`未找到文本为"${option}"的选项。`);
-            }
-
-            console.log(`已选择 "${option}"。`);
-            listItem.click();
-
-            // 返回成功的结果
-            return { success: true, message: `选项 "${option}" 已成功选择。` };
-        } catch (error) {
-            console.error("错误:", error.message);
-            throw error; // 将错误向上抛出，以便外部可以捕获
-        }
-    }
-
-
-    /**
-     * 等待元素出现
-     * @param {string} selector - jQuery选择器
-     * @param {number} timeout - 超时时间
-     * @returns {Promise<jQuery>} - 返回一个Promise对象，包含一个jQuery对象
-     */
-    function waitForElement(selector, timeout = 30000) {
-        if (typeof selector !== 'string' || selector.length === 0) {
-            throw new Error('选择器参数必须是一个非空字符串。');
-        }
-
-        return new Promise((resolve, reject) => {
-            // 首先检查元素是否已存在
-            const existingElements = $(selector);
-            if (existingElements.length > 0) {
-                console.log(`元素已存在: ${selector}`);
-                resolve(existingElements);
-            }
-
-            // 设置 MutationObserver 来观察后续的 DOM 变化
-            const observer = new MutationObserver((mutations) => {
-                const elements = $(selector);
-                if (elements.length > 0) {
-                    console.log(`已找到元素: ${selector}`);
-                    observer.disconnect(); // 找到元素后断开观察
-                    resolve(elements);
-                }
-            });
-
-            observer.observe(document.body, { childList: true, subtree: true });
-
-            // 设置超时处理
-            setTimeout(() => {
-                console.log(`等待元素超时: ${selector}`);
-                observer.disconnect();  // 超时后断开观察
-                reject(new Error(`Timeout waiting for element: ${selector}`));
-            }, timeout);
-        });
-    }
-
+    // 监控对话框的显示与隐藏状态
+    $(document).ready(() => {
+        monitorDialog('.el-dialog__wrapper.page-dialog.all-test-dialog');
+    });
 
 })();
