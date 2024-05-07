bef63c4e12365d68aec3b325e3601c43c6c1d6a6
GitHub Action <action@github.com>
2024-05-07 12:40:20 +0000

    Automated build commit

--- dist/script-last.user.js
+++ dist/script-last.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         体检系统辅助
 // @namespace    http://tampermonkey.net/
-// @version      0.1.29
+// @version      0.1.30
 // @description  监控特定元素属性的变化，并根据变化执行相应的操作。
 // @author       太公摘花
 // @match        https://wx.changx.com/*
@@ -16,7 +16,7 @@
 
 
     /**
-     * 监控对话框的显示与隐藏状态
+     * @summary 监控对话框的显示与隐藏状态
      * 
      * @param {string} selector - jQuery选择器，用于定位需要监控的元素
      * @returns {void} - 无返回值
@@ -30,7 +30,7 @@
             attributeFilter: ['style'] // 只监控style属性
         };
 
-        setupObserver(selector, observerConfig, (element) => {
+        DOMUtils.setupObserver(selector, observerConfig, (element) => {
             const display = element.css('display');
             if (display !== 'none') {
                 console.log('对话框显示，启动标签页监控...');
@@ -51,21 +51,21 @@
 
 class DOMUtils {
     /**
- * 模拟指定标题的下拉框选择指定选项
- * 
- * @param {string} title - 下拉框标题
- * @param {string} option - 选项
- * @returns {Promise<{success: boolean, message: string}>} - 返回一个Promise对象，包含操作结果
- * @throws {Error} - 如果参数不符合要求，将抛出异常
- * @example
- * selectDropdownOption('尿蛋白', '阴性').then(result => {
- *   if (result.success) {
- *    console.log(result.message);
- *  } else {
- *   console.error(result.message);
- * }
- * });
- */
+     * @summary 模拟指定标题的下拉框选择指定选项
+     * 
+     * @param {string} title - 下拉框标题
+     * @param {string} option - 选项
+     * @returns {Promise<{success: boolean, message: string}>} - 返回一个Promise对象，包含操作结果
+     * @throws {Error} - 如果参数不符合要求，将抛出异常
+     * @example
+     * selectDropdownOption('尿蛋白', '阴性').then(result => {
+     *   if (result.success) {
+     *    console.log(result.message);
+     *  } else {
+     *   console.error(result.message);
+     * }
+     * });
+     */
     static async selectDropdownOption(title, option) {
         if (typeof title !== 'string' || typeof option !== 'string') {
             throw new Error('标题和选项参数必须是字符串。');
@@ -74,8 +74,7 @@ class DOMUtils {
         try {
             console.log(`正在查找标题为 "${title}" 的下拉框。`);
             const labelDiv = await this.waitFor(() => $(`div.el-form-item__content`).filter(function () {
-                // 忽略空格和大小写，查找匹配的选项
-                return $.trim($(this).text().replaceAll(" ","").replaceAll("+","").replaceAll("-","")) === title;
+                return $.trim($(this).text().replaceAll(" ", "").replaceAll("+", "").replaceAll("-", "")) === title;
             }));
 
             if (labelDiv.length === 0 || labelDiv.find('input').val() !== '') {
@@ -86,7 +85,18 @@ class DOMUtils {
             labelDiv.children().first().click();
 
             console.log(`正在等待 "${title}" 下拉菜单选项出现。`);
-            const placementDiv = await this.waitFor('div[x-placement="bottom-start"]');
+            const placementDiv = await this.waitFor(
+                'div[x-placement="bottom-start"], div[x-placement="top-start"]',
+                () => true,
+                2000,
+                100,
+                () => {
+                    console.log(`"${title}" 下拉菜单未出现，重新尝试打开。`);
+                    labelDiv.children().first().click();
+                    return DOMUtils.waitFor('div[x-placement="bottom-start"]');
+                }
+            );
+
             const listItem = placementDiv.find('li').filter((i, elem) => $.trim($(elem).text()) === option);
 
             if (listItem.length === 0) {
@@ -105,7 +115,7 @@ class DOMUtils {
     }
 
     /**
-     * 模拟点击指定选择器的每一个元素
+     * @summary 模拟点击指定选择器的每一个元素
      * 
      * @param {string} selector - jQuery选择器，指定需要模拟点击的元素
      * @returns {void} - 无返回值
@@ -122,16 +132,16 @@ class DOMUtils {
     }
 
     /**
-* 点击未被选中的标签
-* 
-* @param {jQuery} label - jQuery对象，表示需要点击的标签
-* @returns {void} - 无返回值
-* @example
-* clickUncheckedLabel($('label'));
-* clickUncheckedLabel($('label').first());
-* clickUncheckedLabel($('label').eq(1));
-* ...
-*/
+    * @summary 点击未被选中的标签
+    * 
+    * @param {jQuery} label - jQuery对象，表示需要点击的标签
+    * @returns {void} - 无返回值
+    * @example
+    * clickUncheckedLabel($('label'));
+    * clickUncheckedLabel($('label').first());
+    * clickUncheckedLabel($('label').eq(1));
+    * ...
+    */
     static clickUncheckedLabel(label) {
         if (!label.parents("tr").find('label.is-checked').length) {
             label.click(); // 如果没有选中的标签，进行点击
@@ -139,10 +149,8 @@ class DOMUtils {
             console.log('已存在被选中的标签，跳过当前操作');
         }
     }
+
     /**
-     * 
-     * 等待元素出现，并满足条件
-     * 
      * @summary 等待元素出现，并满足条件
      * 
      * @param {string|Function} selector - jQuery选择器或返回jQuery对象的函数 
@@ -162,45 +170,47 @@ class DOMUtils {
      * @see {@link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise}
      * @see {@link https://api.jquery.com/jQuery/}
      */
-    static waitFor(selector, condition = () => true, timeout = 30000, interval = 100) {
-
+    static waitFor(
+        selector, 
+        condition = () => true, 
+        timeout = 30000, 
+        interval = 100, 
+        callbackIfTimeout = () => { throw new Error('等待条件满足超时。'); }
+    ) {
         return new Promise((resolve, reject) => {
-            if (typeof selector !== 'string' && typeof selector !== 'function') {
-                reject(new Error('参数selector必须是字符串或返回jQuery对象的函数。'));
-            }
-            if (typeof condition !== 'function') {
-                reject(new Error('condition不是一个有效的function。'));
-            }
-            if (typeof timeout !== 'number' || typeof interval !== 'number') {
-                reject(new Error('timeout和interval必须为number。'));
-            }
-
             const startTime = Date.now();
-
-            // 检查条件是否满足
+            
             function checkCondition() {
                 let $element = typeof selector === 'function' ? selector() : $(selector);
                 if (!($element instanceof jQuery)) {
-                    reject(new Error("选择器或函数没有返回一个jQuery对象。"));
+                    return reject(new Error("选择器或函数没有返回一个 jQuery 对象。"));
                 }
 
-                // 检查jQuery选择器是否存在且满足条件，满足则返回一个jQuery对象，否则继续等待，直到超时
+                // 满足条件或超时
                 if ($element.length > 0 && condition($element)) {
                     resolve($element);
                 } else if (Date.now() - startTime > timeout) {
-                    reject(new Error('等待条件满足超时。'));
+                    try {
+                        const result = callbackIfTimeout();
+                        if (result instanceof Promise) {
+                            result.then(resolve).catch(reject);
+                        } else {
+                            reject(new Error('等待条件满足超时。'));
+                        }
+                    } catch (error) {
+                        reject(error);
+                    }
                 } else {
                     setTimeout(checkCondition, interval);
                 }
             }
-
+            
             checkCondition();
         });
     }
 
+
     /**
-     * 设置元素属性变化的观察者
-     * 
      * @summary 设置元素属性变化的观察者
      * 
      * @param {string} selector - jQuery选择器，用于定位需要监控的元素
@@ -230,7 +240,29 @@ class DOMUtils {
     }
 }
 
+
+/**
+ * @summary 监控特定元素属性的变化，并根据变化执行相应的操作。
+ * 
+ * @class ElementObserver
+ * @example
+ * const observer = new ElementObserver('#tab-0', 'tabindex', 0, performTab0Actions);
+ * observer.start();
+ * ...
+ */
 class ElementObserver {
+    /**
+     * @summary 创建一个ElementObserver实例
+     * 
+     * @param {string} selector - jQuery选择器，用于定位需要监控的元素
+     * @param {string} attribute - 需要监控的属性
+     * @param {string|number} value - 属性的值
+     * @param {Function} action - 当属性变化时执行的回调函数
+     * @example
+     * const observer = new ElementObserver('#tab-0', 'tabindex', 0, performTab0Actions);
+     * ...
+     * @see {@link https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver}
+     */
     constructor(selector, attribute, value, action) {
         this.selector = selector;
         this.attribute = attribute;
@@ -239,34 +271,89 @@ class ElementObserver {
         this.observer = null;
     }
 
+    /**
+     * @summary 启动监控
+     * 
+     * @returns {Promise<void>} - 返回一个Promise对象
+     * @example
+     * observer.start();
+     * ...
+     */
     async start() {
         const $element = await DOMUtils.waitFor(this.selector);
         this.#runAction($element);
         this.observer = await DOMUtils.setupObserver(this.selector, { attributes: true, attributeFilter: [this.attribute] }, this.#runAction.bind(this));
     }
 
+    /**
+     * @summary 停止监控
+     * 
+     * @returns {void} - 无返回值
+     * @example
+     * observer.stop();
+     * ...
+     * @see {@link https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver/disconnect}
+     */
     stop() {
         if (this.observer) {
             this.observer.disconnect();
             this.observer = null;
         }
     }
+
+    /**
+     * @summary 执行回调函数
+     * 
+     * @private
+     * @param {jQuery} $element - jQuery对象，表示需要执行回调函数的元素
+     * @returns {void} - 无返回值
+     * @example
+     * #runAction($element);
+     * ...
+     */
     #runAction($element) {
-        if ($element.attr(this.attribute) == this.value){
+        if ($element.attr(this.attribute) == this.value) {
             this.action($element);
         }
     }
 }
 
 class ObserverManager {
+
+    /**
+     * @summary 创建一个ObserverManager实例
+     * 
+     * @example
+     * const manager = new ObserverManager();
+     * ...
+     */
     constructor() {
         this.observers = {};
     }
 
+    /**
+     * @summary 添加一个观察者
+     * 
+     * @param {string} id - 观察者的ID
+     * @param {ElementObserver} observer - ElementObserver实例
+     * @returns {void} - 无返回值
+     * @example
+     * manager.add('tab0', observer);
+     * ...
+     */
     add(id, observer) {
         this.observers[id] = observer;
     }
 
+    /**
+     * @summary 移除一个观察者
+     * 
+     * @param {string} id - 观察者的ID
+     * @returns {void} - 无返回值
+     * @example
+     * manager.remove('tab0');
+     * ...
+     */
     remove(id) {
         if (this.observers[id]) {
             this.observers[id].stop();
@@ -274,27 +361,62 @@ class ObserverManager {
         }
     }
 
+    /**
+     * @summary 启动指定观察者
+     * 
+     * @param {string} id - 观察者的ID
+     * @returns {void} - 无返回值
+     * @example
+     * manager.start('tab0');
+     * ...
+     */
     start(id) {
         if (this.observers[id]) {
             this.observers[id].start();
         }
     }
 
+    /**
+     * @summary 停止指定观察者
+     * 
+     * @param {string} id - 观察者的ID
+     * @returns {void} - 无返回值
+     * @example
+     * manager.stop('tab0');
+     * ...
+     */
     stop(id) {
         if (this.observers[id]) {
             this.observers[id].stop();
         }
     }
 
+    /**
+     * @summary 启动所有观察者
+     * 
+     * @returns {void} - 无返回值
+     * @example
+     * manager.startAll();
+     * ...
+     */
     startAll() {
         Object.values(this.observers).forEach(observer => observer.start());
     }
 
+    /**
+     * @summary 停止所有观察者
+     * 
+     * @returns {void} - 无返回值
+     * @example
+     * manager.stopAll();
+     * ...
+     */
     stopAll() {
         Object.values(this.observers).forEach(observer => observer.stop());
     }
 }
 const actions = {
+    // 标签页 0 的操作
     performTab0Actions: async () => {
         let urinalysisItems = ["尿蛋白", "尿糖", "尿酮体", "尿潜血", "白细胞"];
 
@@ -312,17 +434,20 @@ const actions = {
             }
         }
     },
+    // 标签页 1 的操作
     performTab1Actions: () => {
         console.log('标签页 1 激活，执行操作...');
         DOMUtils.simulateClick('div#pane-1 input[type="radio"][value="1"]');
         DOMUtils.clickUncheckedLabel($('div#pane-1 input[type="radio"][value="4"]').first().closest('label'));
     },
+    // 标签页 2 的操作
     performTab2Actions: () => {
         console.log('标签页 2 激活，执行操作...');
         DOMUtils.simulateClick('div#pane-2 input[type="radio"][value="0"]');
     }
 };
 
+// 创建观察者管理器
 const manager = new ObserverManager();
 Object.keys(actions).forEach((key, index) => {
     const observer = new ElementObserver(`#tab-${index}`, 'tabindex', 0, actions[key]);
